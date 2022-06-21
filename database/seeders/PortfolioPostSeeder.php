30ebd85cddca8bd21af14d3e612b3df2a43f9521
asmi046 <asmi046@gmail.com>
2022-06-21 17:08:32 +0300

    + в портфолио

--- database/seeders/PortfolioPostSeeder.php
+++ database/seeders/PortfolioPostSeeder.php
@@ -29,8 +29,8 @@ class PortfolioPostSeeder extends Seeder
                 'color2' => '#FFFFFF',
                 'color3' => '#005d42',
                 'color4' => '#c41c35',
-                'oblozgka' => 'vt.jpg',
-                'page_prev' => '1vtorg-present.webp' 
+                'oblozgka' => '1vtorg-present.webp',
+                'page_prev' => 'vt.jpg' 
             ]);
 
             DB::table("portfolio_posts")->insert(
@@ -46,6 +46,36 @@ class PortfolioPostSeeder extends Seeder
                     'color4' => '#144970',
                     'oblozgka' => 'vt.jpg',
                     'page_prev' => 'autotish-obl.webp' 
+                ]);
+
+            DB::table("portfolio_posts")->insert(
+                [
+                    'name' => 'Cайта для клубного дома "Боева Дача"',
+                    'image_folder_name' => 'boeva',
+                    'seo_title' => 'Кейс: Концепт презентационного сайта для клубного дома Боева Дача - Asmi-Studio',
+                    'seo_description' => 'Разработка дизайна и концепта навигации сайта для клубного дома Боева Дача. Создание сайта компании не дорого.',
+                    'content' => 'В рамках проекта был разработан концепт дизайна презентационного сайта клубного дома "Боева Дача". Так же был проработан и реализован принцип напвигации между разделами сайта.',
+                    'color1' => '#8a754a',
+                    'color2' => '#FFFFFF',
+                    'color3' => '#dfba6a',
+                    'color4' => '',
+                    'oblozgka' => '',
+                    'page_prev' => 'boeva-present.webp' 
+                ]);
+
+            DB::table("portfolio_posts")->insert(
+                [
+                    'name' => 'Lending Page для продажи реплик наушников CLR PODS',
+                    'image_folder_name' => 'clrpds',
+                    'seo_title' => 'Кейс: Разработка Lending Page для продажи реплик популярных наушников - Asmi-Studio',
+                    'seo_description' => 'Проект по созданию посадочной страницы для продажи реплик TWS наушников. Разработка Lending Page в Курске',
+                    'content' => 'В рамках проекта создана посадочная страница для продажи реплик безпроводных наушников под брендом CLR PODS. Конверсия трафика из социальных сетей 7%.',
+                    'color1' => '#f8859e',
+                    'color2' => '#FFFFFF',
+                    'color3' => '#89d0ce',
+                    'color4' => '#4aab4d',
+                    'oblozgka' => '',
+                    'page_prev' => 'clrpds-present.webp' 
                 ]);
 
     }
