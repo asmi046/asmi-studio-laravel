909bb867c8e056e32aef54421f9071afdb0fcb4b
asmi046 <asmi046@gmail.com>
2022-07-20 20:00:27 +0300

    Отправщик в телеграмм

--- app/Http/Controllers/SenderController.php
+++ app/Http/Controllers/SenderController.php
@@ -10,6 +10,35 @@ use Illuminate\Support\Facades\Mail;
 
 class SenderController extends Controller
 {
+
+    protected function message_to_telegram($text)
+    {
+        $arr_chat = "381762556,845697964,496378528"; 
+        if($arr_chat) {
+
+            $arr_chat = explode(",",$arr_chat);
+            $ch = curl_init();
+            
+            for ($i = 0; $i<count($arr_chat); $i++) {
+                curl_setopt_array(
+                    $ch,
+                    array(
+                        CURLOPT_URL => 'https://api.telegram.org/bot' . env("TG_TOKEN") . '/sendMessage',
+                        CURLOPT_POST => TRUE,
+                        CURLOPT_RETURNTRANSFER => TRUE,
+                        CURLOPT_TIMEOUT => 10,
+                        CURLOPT_POSTFIELDS => array(
+                            'chat_id' => trim($arr_chat[$i]),
+                            'text' => $text,
+                        ),
+                    )
+                );
+                $output = curl_exec($ch);
+            }
+        }
+        
+    }
+
     public function send_contact_form(ContactForm $request) {
         Mail::to("asmi046@gmail.com")->send(new ContactFormMail($request));
         return redirect(route("thanks"));
