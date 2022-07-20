5aba822f0fc461d6c0524bb0d811666f7242e376
asmi046 <asmi046@gmail.com>
2022-07-20 10:18:44 +0300

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.all')
+
+@section('title', "Благодарим за обращение - Asmi-Studio")
+@section('description', "Благодарим за обращение в нашу студию")
+
+@section('content')
+    <section class = "all_portfolio">
+        <div class = "content">
+            <h1>404</h1>
+            <p>Хм что то как то не то вообщем!</p>
+        </div>
+    </section>
+@endsection
