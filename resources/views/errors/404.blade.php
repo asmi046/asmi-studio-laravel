6f657c2f07019cfba3e9e19e33e4083ed51404de
asmi046 <asmi046@gmail.com>
2022-07-21 14:31:58 +0300

    404 и страница благодарности

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,10 +4,24 @@
 @section('description', "Благодарим за обращение в нашу студию")
 
 @section('content')
-    <section class = "all_portfolio">
+    <section class = "p404 full_h all_portfolio">
         <div class = "content">
-            <h1>404</h1>
-            <p>Хм что то как то не то вообщем!</p>
+            
+
+            <div class="wrap2cel">
+                <div class = "cell">
+                    <h1>404</h1>
+                    <p>Хм что то как то не то вообщем!</p>
+                    <a href="{{route('home')}}" class="button">Вернуться на главную</a>
+                    <br>
+                    <br>
+                    <a href="{{route('portfolio')}}" class="button">Смотреть портфолио</a>
+                </div>
+                <div class = "cell">
+                    <img class = "main_img" src="{{asset("img/cone.jpg")}}" alt="Спасибо за обращение">
+                </div>
+            </div>
+
         </div>
     </section>
 @endsection
--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -6,9 +6,22 @@
 @section('img_url', asset('img/komp-bg-desctop.webp'))
 
 @section('content')
-    <section class = "all_portfolio">
+    <section class = "thencs full_h">
         <div class = "content">
-            Спсибо за обращение!
+            <div class="wrap2cel">
+                <div class = "cell">
+                    <h1>Благодарим за обращение</h1>
+                    <p>Наши специалисты свяжутся с вами в течении 15 минут.</p>
+                    <a href="{{route('home')}}" class="button">Вернуться на главную</a>
+                    <br>
+                    <br>
+                    <a href="{{route('portfolio')}}" class="button">Смотреть портфолио</a>
+                </div>
+                <div class = "cell">
+                    <img class = "main_img" src="{{asset("img/15min.jpg")}}" alt="Спасибо за обращение">
+                </div>
+            </div>
+
         </div>
     </section>
 @endsection
