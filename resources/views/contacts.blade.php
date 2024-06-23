fbb7a1b48731ac8f05c2d8fbe047d89315e6d4a3
asmi046 <asmi046@gmail.com>
2024-06-23 14:00:37 +0300

    Убрал имя из формы

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,11 +4,11 @@
 @section('description', "Свяжитесь с нами любым удобным для Вас смособом и мы поможем Вам разработать эффективный сайт приносящий прибыль.")
 
 @section('content')
-	
+
     <section class = "all_portfolio">
         <div class = "content">
             <div class = "breadcrumbs" id = "breadcrumbs">
-                <a href = "{{ route('home') }}">Главная</a> / <span>Контакты</span> 
+                <a href = "{{ route('home') }}">Главная</a> / <span>Контакты</span>
             </div>
 
             <h1>Контакты</h1>
@@ -16,60 +16,61 @@
             <div class="wrap2cel">
                 <div class="cell">
                     <p>
-                        <a href = "tel:+79036330801">+7 903 633 08 01</a>	
+                        <a href = "tel:+79036330801">+7 903 633 08 01</a>
                     </p>
-                    
+
                     <p>
-                        <a href = "mailto:info@asmi-studio.ru">info@asmi-studio.ru</a>	
+                        <a href = "mailto:info@asmi-studio.ru">info@asmi-studio.ru</a>
                     </p>
-                    
-                    
+
+
                     <div class = "socicon_in_footer socicon_in_footer_contact_page">
                         @include('parts.soc_lnk')
-                    </div>	
-                    
-                    
+                    </div>
+
+
                     <h2>Реквизиты</h2>
                     <p>
                         ИП Смирнов Андрей Алексеевич<br/>
                         ИНН: 463246349734<br/>
                         ОГРН: 313463235300020<br/>
-                    </p>                    
+                    </p>
                 </div>
 
                 <div class="cell">
-                    <h2>Обратная связь</h2>
+                    {{-- <h2>Обратная связь</h2>
                     <form method="POST" action="{{ route("send_contact_form") }}">
                         @csrf
-        
+
                         <input type="hidden" name = "form_name" value = "Форма в контактах">
                         <input type="hidden" name = "form_rasp" value = "Страница контактов">
                         <input type="hidden" name = "form_address"  value = "{{url()->current()}}">
-        
-                        <input type="text" name = "name" placeholder="Введите ваше имя*" class = "@error('name') input_error @enderror"  value = "{{ old('name') }}">        
+
+                        <input type="text" name = "name" placeholder="Введите ваше имя*" class = "@error('name') input_error @enderror"  value = "{{ old('name') }}">
                         <input type="tel" name = "phone" placeholder="Введите телефон*" class = "@error('phone') input_error @enderror" value = "{{ old('phone') }}">
 
-        
+
                         <button type = "submit" class = "button">Отправить</button>
-                    </form>
+                    </form> --}}
                 </div>
+
             </div>
-            
-            
-            
 
 
-            
+
+
+
+
 
             <h2>Как добраться</h2>
             <div id = "mapLine" class = "mapLine"></div>
-            
+
             <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
             <script>
                 ymaps.ready(init);
 
                 function init () {
-                
+
                 var myMap = new ymaps.Map("mapLine", {
                         center: [51.72769457226839,36.164475499999966],
                         zoom: 14,
@@ -86,18 +87,18 @@
                         iconImageSize: [30, 54],
                         iconImageOffset: [-15, -54]
                     });
-                    
+
                     myMap.geoObjects.add(myPlacemarkAdr);
-                    
-                    
-                    
-                    
+
+
+
+
 
 
                     myMap.behaviors.disable('scrollZoom');
                 }
             </script>
-            
+
         </div>
     </section>
 
--- resources/views/parts/popup.blade.php
+++ resources/views/parts/popup.blade.php
@@ -1,7 +1,7 @@
 <div class="popup popup_consult">
 	<div class="popup__content">
 		<div class="popup__body">
-			<div class="popup__close" aria-label="Закрыть модальное окно"></div> 
+			<div class="popup__close" aria-label="Закрыть модальное окно"></div>
 			<div class="popup__item d-flex">
 				<div class="popup__img">
 					<picture>
@@ -9,8 +9,8 @@
 							<img loading="lazy" src="{{asset('/img/popup.jpg')}}" alt="">
 					</picture>
 				</div>
-				<div class="popup__form-block">  
-					<h2>Заказать звонок</h2> 
+				<div class="popup__form-block">
+					<h2>Заказать звонок</h2>
 
 						<p class="popup__notific">Оставьте заявку и мы свяжемся с Вами в течении 15 минут</p>
 						<form id="request_call" action="{{ route("send_contact_form") }}" class="form" method="POST">
@@ -21,14 +21,13 @@
 								<input type="hidden" name = "form_name" value = "Заказать звонок">
 								<input type="hidden" name = "form_rasp" value = "По умолчанию">
 								<input type="hidden" name = "form_address" value = "{{url()->current()}}">
-								<input required type="text" name="name" placeholder="Имя" class="input_name popup__form-input input">
-								<input required type="tel" name="phone" placeholder="Телефон" class="input_phone popup__form-input input _phone"> 
-								
+								<input required type="tel" name="phone" placeholder="Телефон" class="input_phone popup__form-input input _phone">
+
 							</div>
 							<p class="popup__policy">Заполняя данную форму вы соглашаетесь с <a href="#">политикой
 									конфиденциальности</a></p>
-							
-								<div class="popup_btn_wrapper">	
+
+								<div class="popup_btn_wrapper">
 									<button type = "submit" class="button new_send_btn" data-formid = "request_call">Отправить</button>
 									<img loading="lazy" src="{{asset('/img/icon/loader.svg')}}" alt="">
 								</div>
